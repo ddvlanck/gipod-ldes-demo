2f943b6abc3b3418351faf201a8eca24a0111984
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2022-08-04 17:24:22 +0200

    Update url to ldes in config

--- bin/runner.ts
+++ bin/runner.ts
@@ -6,9 +6,11 @@ const run = async (): Promise<void> => {
   const raw = await readFile('config.json', 'utf-8');
   const config = JSON.parse(raw);
 
+  const database = process.env.DATABASE || config.database;
+  const url = process.env.LDES || config.ldes;
+
   const app = new AppRunner();
-  await app.create(config.database, config.url);
-  await app.run();
+  await app.create(database, url).then(() => app.run());
 }
 
 run().catch(error => console.error(error));
